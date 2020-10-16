9954914a3555e2fe80c246c0d8a68c6e352228c7
33 <33@example.com>
2020-10-16 14:29:07 +0900

    'チャット機能追加'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def top
+    @text = Text.all
   end
+  def create
+    @text = Text.new(text: params[:message])
+    @text.save
+    redirect_to '/'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'home/top'
   get'/', to: 'home#top'
+  post '/home/create', to: 'home#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
