f203748f64a533b64c0aca67d6677932a9fc5ee3
13 <13@example.com>
2020-11-04 11:07:36 +0900

    画像

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,20 +46,6 @@ class UsersController < ApplicationController
     end
   end
 
-  #プロフィールもいる？
-  def create
-    @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
