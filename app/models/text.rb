f6c7c41bd73d2e1f6528e2df3d7b379cd66485f3
33 <33@example.com>
2020-12-09 12:28:07 +0900

    自動更新機能の作成

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,3 +1,5 @@
 class Text < ApplicationRecord
     belongs_to :room
+    validates :text, presence: true #これにより無記入投稿とエンター長押しの連続投稿の2つが同時に防げる
+    after_create_commit { MessageBroadcastJob.perform_later self }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  
   resources :rooms
  
+  mount ActionCable.server => '/cable'
 
   #get 'home/top'
   root 'home#top'
